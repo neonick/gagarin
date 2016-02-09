4cdd367c7476b7cd2507d3a0cc06f7c44515f80f
Nickolay Marchenko <n30n1ck@gmail.com>
2016-02-09 16:27:52 +0300

    0.4.9

--- src/js/main.js
+++ src/js/main.js
@@ -2,37 +2,76 @@
     Custom scripts
  */
 //= ../../bower_components/jquery/dist/jquery.min.js
-//= ../../node_modules/nprogress/nprogress.js
-//= ../../bower_components/magnific-popup/dist/jquery.magnific-popup.min.js
 //= ../../bower_components/Swiper/dist/js/swiper.jquery.min.js
-//= ../../bower_components/jquery.scrollTo/jquery.scrollTo.min.js
-//= ../../bower_components/Tabslet/jquery.tabslet.min.js
 //= ../../bower_components/jquery.maskedinput/dist/jquery.maskedinput.min.js
-//= ../../bower_components/ion.rangeslider/js/ion.rangeSlider.min.js
-//= ../../bower_components/fotorama/fotorama.js
+
+//|=== unkomment to use ===| //= ../../node_modules/nprogress/nprogress.js
+//|=== unkomment to use ===| //= ../../bower_components/magnific-popup/dist/jquery.magnific-popup.min.js
+//|=== unkomment to use ===| //= ../../bower_components/jquery.scrollTo/jquery.scrollTo.min.js
+//|=== unkomment to use ===| //= ../../bower_components/Tabslet/jquery.tabslet.min.js
+//|=== unkomment to use ===| //= ../../bower_components/ion.rangeslider/js/ion.rangeSlider.min.js
+//|=== unkomment to use ===| //= ../../bower_components/fotorama/fotorama.js
 
 $(document).ready(function () {
 
     // do something
-    
-    // masked input
+
+    // init masked input
     $("#phone").mask("(999) 999-99-99");
 
-    //= parts/_init_gmaps.js
+    // init Swiper
+    var swiper = new Swiper('.swiper-container', {
+       pagination: '.swiper-pagination',
+       paginationClickable: true,
+       nextButton: '.swiper-button-next',
+       prevButton: '.swiper-button-prev',
+       spaceBetween: 30
+    });
 
-    //= parts/_init_swiper.js
+
+    // initialize map
+    function initialize() {
+        var mapLat = $("#map").data('lat'),
+            mapLng = $("#map").data('lng'),
+            mapIcon = $("#map").data('mapicon');
+
+        var pos = new google.maps.LatLng(mapLat, mapLng),
+            centr = new google.maps.LatLng(mapLat, mapLng),
+            zoomNum;
+
+        $(window).width()>768 ? zoomNum = 12 : zoomNum = 14;
+
+        var mapOptions = {
+            center: centr,
+            zoom: zoomNum,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            mapTypeControlOptions: {
+                style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+            },
+            // disableDefaultUI: true,
+            scrollwheel: false,
+            rotateControl: true,
+            styles: // https://snazzymaps.com/
+                    [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}]
+        };
+
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+        map.setTilt(45);
+
+        var marker = new google.maps.Marker({
+            position: pos,
+            map: map,
+            title: '',
+            animation: google.maps.Animation.DROP,
+            icon: {
+                url: 'i/' + mapIcon,
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(48, 100)
+            }
+        });
+
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
+
 
 });
-
-
-$(document).ready(function () {
-	// start NProgress
-	NProgress.start();
-});
-
-$(window).load(function() {
-
-	// stop NProgress
-	NProgress.done();
-
-})
--- src/js/parts/_init_swiper.js
+++
@@ -1,9 +0,0 @@
-// init Swiper http://www.idangero.us/swiper/get-started/
-
-var swiper = new Swiper('.swiper-container', {
-   pagination: '.swiper-pagination',
-   paginationClickable: true,
-   nextButton: '.swiper-button-next',
-   prevButton: '.swiper-button-prev',
-   spaceBetween: 30
-});
